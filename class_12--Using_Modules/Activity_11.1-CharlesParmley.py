a23f55990358f55182cbc63133356e7e42767cb4
charles <charleshparmley@icloud.com>
2021-09-28 14:25:09 -0500

    Activity 11.1

---
+++ class_12--Using_Modules/Activity_11.1-CharlesParmley.py
@@ -0,0 +1,16 @@
+# Create a function that generates a random list
+# It should take in a number that is the size of the list.
+# The values passed in should be between 20 and 78.
+
+from random import randrange
+
+def random_list_generator(list_length = 5):
+    result_list = []
+
+    # Iterate a specified amount of times
+    for number in range(0,list_length):
+        # Only add random numbers between 19 and 78
+        result_list.append(randrange(20,78))
+    print(result_list)
+
+random_list_generator(1000)
