8b07c0d5a13d62550d746e1fb6ab6c52e8fee0ab
charles <charleshparmley@icloud.com>
2021-12-07 14:33:42 -0600

    JQuery endless scrollbox

--- PY-113: Intro Web-Dev/class_10--JQuery/Bottomless_Scroll.js
+++ PY-113: Intro Web-Dev/class_10--JQuery/Bottomless_Scroll.js
@@ -3,16 +3,18 @@
 
 let counter = 1
 function makePost(){
+    // create new p-tag element
     let recurringText = document.createElement('p')
-
+    // set its text to the counter number
     recurringText.innerText = `Post #`+counter
+    // increase counter
     counter++
+    // return the resulting p-tag
     return recurringText
 }
 
 // Listener for the scrollbox
-
 $('.bottomless-scrollbox').on('scroll', function(){
-    console.log('scrolling')
+    // call the post maker function and add its result to the DOM
     $('.bottomless-scrollbox').append(makePost())
 })
